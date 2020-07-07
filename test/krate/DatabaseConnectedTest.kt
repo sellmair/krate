42bb903db9025615265e8562d6e0b8e0c21378c9
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-07-07 11:39:46 -0400

    test: add class for database connected tests

---
+++ test/krate/DatabaseConnectedTest.kt
@@ -0,0 +1,49 @@
+package krate
+
+import epgx.models.PgTable
+
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.ExtensionContext
+
+@ExtendWith(DatabaseConnectedTest::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+open class DatabaseConnectedTest(private vararg val tables: PgTable = arrayOf())
+    : BeforeEachCallback, AfterEachCallback {
+
+    @BeforeAll
+    fun setUpDatabase() {
+        val dbUrl = System.getenv("KRATE_TEST_DB_URL") ?: error("KRATE_TEST_DB_URL not set")
+
+        val dbUser = System.getenv("KRATE_TEST_DB_USER") ?: error("KRATE_TEST_DB_USER not set")
+
+        val dbPassword = System.getenv("KRATE_TEST_DB_PASSWORD") ?: error("KRATE_TEST_DB_PASSWORD not set")
+
+        Database.connect(dbUrl, "org.postgresql.Driver", dbUser, dbPassword)
+
+        transaction { SchemaUtils.drop(*tables) }
+        transaction { SchemaUtils.create(*tables) }
+    }
+
+    override fun beforeEach(context: ExtensionContext?) {
+        val name = context?.testMethod?.get()?.name ?: error("test method is null")
+
+        println("=== [ Test : $name ] ===\n")
+    }
+
+    override fun afterEach(context: ExtensionContext?) = println()
+
+    @AfterAll
+    fun tearDownTables() {
+        transaction { SchemaUtils.drop(*tables) }
+    }
+
+}
