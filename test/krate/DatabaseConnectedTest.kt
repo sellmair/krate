8cc45fdcb45c45d8312ca145564796275b5f8dbc
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-07-07 22:05:05 -0400

    test: make DatabaseConnectedTest include queryContext(), blockingTransaction {}

--- test/krate/DatabaseConnectedTest.kt
+++ test/krate/DatabaseConnectedTest.kt
@@ -1,9 +1,17 @@
 package krate
 
-import epgx.models.PgTable
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+
+import krate.models.DatabaseContext
+import krate.models.QueryContext
+import krate.models.Repository
+import krate.util.MapCache
+
+import reflectr.entity.Entity
 
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
 import org.junit.jupiter.api.AfterAll
@@ -14,10 +22,29 @@ import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.ExtensionContext
 
+import epgx.models.PgTable
+
+import kotlinx.coroutines.runBlocking
+import kotlin.reflect.KClass
+
+import java.util.*
+
 @ExtendWith(DatabaseConnectedTest::class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 open class DatabaseConnectedTest(private vararg val tables: PgTable = arrayOf())
     : BeforeEachCallback, AfterEachCallback {
+
+    fun <T> blockingTransaction(block: suspend Transaction.() -> T) = transaction { runBlocking { block() } }
+
+    fun queryContext() = object : QueryContext {
+        override val databaseContext = object : DatabaseContext {
+            override val repoCache = MapCache<KClass<out Entity>, Repository<Entity>>()
+        }
+
+        override val entityCache = MapCache<UUID, Entity>()
+
+        override val objectMapper = jacksonObjectMapper()
+    }
 
     @BeforeAll
     fun setUpDatabase() {
